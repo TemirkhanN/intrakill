82924062d9e9d3f09c355d88dba9a3c244ba48ab
Temirkhan <temirkhan@nasukhov.me>
2026-02-06 23:46:30 +0100

    Added tags expand/collapse toggle

--- composeApp/src/commonMain/kotlin/me/nasukhov/intrakill/scene/TagList.kt
+++ composeApp/src/commonMain/kotlin/me/nasukhov/intrakill/scene/TagList.kt
@@ -1,40 +1,53 @@
 package me.nasukhov.intrakill.scene
 
-import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.layout.FlowRow
 import me.nasukhov.intrakill.AppEvent
 import me.nasukhov.intrakill.LocalEventEmitter
+import androidx.compose.foundation.layout.FlowRow
 
 @Composable
 fun TagList(
-    tags: List<String>
+    tags: List<String>,
+    initiallyVisible: Int = 15,
 ) {
     val eventEmitter = LocalEventEmitter.current
     var selectedTags by remember { mutableStateOf<Set<String>>(emptySet()) }
+    var expanded by remember { mutableStateOf(false) }
 
-    FlowRow(
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
-        verticalArrangement = Arrangement.spacedBy(0.dp)
-    ) {
-        tags.forEach { tag ->
-            val selected = tag in selectedTags
+    val visibleTags = if (expanded) tags else tags.take(initiallyVisible)
 
-            FilterChip(
-                selected = selected,
-                onClick = {
-                    selectedTags =
-                        if (selected) selectedTags - tag
-                        else selectedTags + tag
+    Column(modifier = Modifier.fillMaxWidth()) {
+        FlowRow(
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalArrangement = Arrangement.spacedBy(0.dp)
+        ) {
+            visibleTags.forEach { tag ->
+                val selected = tag in selectedTags
 
-                    eventEmitter.emit(AppEvent.TagsSelected(selectedTags.toList()))
-                },
-                label = {
-                    Text(tag)
-                }
+                FilterChip(
+                    selected = selected,
+                    onClick = {
+                        selectedTags =
+                            if (selected) selectedTags - tag
+                            else selectedTags + tag
+
+                        eventEmitter.emit(AppEvent.TagsSelected(selectedTags.toList()))
+                    },
+                    label = { Text(tag) }
+                )
+            }
+        }
+
+        if (tags.size > initiallyVisible) {
+            ElevatedFilterChip(
+                onClick = { expanded = !expanded },
+                selected = false,
+                elevation = FilterChipDefaults.elevatedFilterChipElevation(elevation = 8.dp),
+                label = { Text(if (expanded) "less tags" else "more tags") },
             )
         }
     }
