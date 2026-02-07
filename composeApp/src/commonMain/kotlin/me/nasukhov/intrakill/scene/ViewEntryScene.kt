395c03fa152007e0846e2b5c6076e74184e5cc39
Temirkhan <temirkhan@nasukhov.me>
2026-02-07 23:35:47 +0100

    Minor caching for entry scene

--- composeApp/src/commonMain/kotlin/me/nasukhov/intrakill/scene/ViewEntryScene.kt
+++ composeApp/src/commonMain/kotlin/me/nasukhov/intrakill/scene/ViewEntryScene.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -22,7 +23,10 @@ import me.nasukhov.intrakill.storage.MediaKind
 
 @Composable
 fun ViewEntryScene(entryId: String) {
-    val entry = MediaRepository.getById(entryId)
+    val entry = remember(entryId) {
+        MediaRepository.getById(entryId)
+    }
+
     val eventEmitter = LocalEventEmitter.current
 
     LazyColumn(
