c348a4b6094b038b0d1ce97e157ad963090087a3
Temirkhan <temirkhan@nasukhov.me>
2026-02-20 01:43:45 +0100

    Added fullscreen support for video player on jvm

--- composeApp/src/jvmMain/kotlin/me/nasukhov/intrakill/main.kt
+++ composeApp/src/jvmMain/kotlin/me/nasukhov/intrakill/main.kt
@@ -1,7 +1,11 @@
 package me.nasukhov.intrakill
 
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import chaintech.videoplayer.util.LocalWindowState
 import com.arkivanov.decompose.DefaultComponentContext
 import com.arkivanov.essenty.lifecycle.LifecycleRegistry
 import me.nasukhov.intrakill.navigation.DefaultRootComponent
@@ -10,10 +14,14 @@ fun main() = application {
     val lifecycle = LifecycleRegistry()
     val root = DefaultRootComponent(DefaultComponentContext(lifecycle))
 
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "intrakill",
-    ) {
-        App(root)
+    val windowState = rememberWindowState(width = 900.dp, height = 700.dp)
+    CompositionLocalProvider(LocalWindowState provides windowState) {
+        Window(
+            onCloseRequest = ::exitApplication,
+            state = windowState,
+            title = "intrakill",
+        ) {
+            App(root)
+        }
     }
 }
