fa70753e38cba66c77f52b2514e398df906f9755
Temirkhan <temirkhan@nasukhov.me>
2026-03-05 08:26:42 +0100

    Refactoring api calls from db import

--- composeApp/src/jvmMain/kotlin/me/nasukhov/intrakill/storage/DbImporter.jvm.kt
+++ composeApp/src/jvmMain/kotlin/me/nasukhov/intrakill/storage/DbImporter.jvm.kt
@@ -7,43 +7,49 @@ import java.io.File
 import java.net.HttpURLConnection
 import java.net.URL
 
+private data class Request(private val url: URL, private val authToken: String) {
+    companion object {
+        fun getDbDump(ip: String, port: Int, password: String) = Request(
+            URL("http://$ip:$port/dump"), Security.hash(password)
+        )
+    }
+
+    fun <R> exec(block: HttpURLConnection.() -> R): R {
+        return (url.openConnection() as HttpURLConnection).apply {
+            connectTimeout = 5000
+            readTimeout = 300000
+            requestMethod = "GET"
+            setRequestProperty("Authorization", authToken)
+        }.block()
+    }
+}
+
 actual object DbImporter {
+    private val db = SecureDatabase
 
     actual suspend fun importDatabase(ip: String, port: Int, password: String): Boolean =
         withContext(Dispatchers.IO) {
-            try {
-                val url = URL("http://$ip:$port/dump")
+            val request = Request.getDbDump(ip, port, password)
 
-                val token = Security.hash(password)
-
-                val connection = (url.openConnection() as HttpURLConnection).apply {
-                    connectTimeout = 5000
-                    readTimeout = 300000
-                    requestMethod = "GET"
-
-                    setRequestProperty("Authorization", token)
-                }
-
-                when (connection.responseCode) {
+            request.exec {
+                when (responseCode) {
                     HttpURLConnection.HTTP_OK -> {
                         val tmpFile = File.createTempFile("intrakill_", "_unencrypted.db").also { it.deleteOnExit() }
-                        connection.inputStream.use { input ->
+                        inputStream.use { input ->
                             tmpFile.outputStream().use { output ->
                                 input.copyTo(output)
                             }
                         }
 
-                        SecureDatabase.importFromFile(tmpFile, password).also { tmpFile.delete() }
+                        db.importFromFile(tmpFile, password).also { tmpFile.delete() }
                     }
                     HttpURLConnection.HTTP_FORBIDDEN -> {
                         throw IllegalStateException("Import failed: Incorrect password/token rejected by server.")
                     }
                     else -> {
-                        throw IllegalStateException("Import failed: Server returned ${connection.responseCode}")
+                        throw IllegalStateException("Import failed: Server returned $responseCode")
                     }
                 }
-            } catch (e: Exception) {
-                throw e
             }
         }
 }
